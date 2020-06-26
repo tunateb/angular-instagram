7e1b64839eb8473e7456641b1d4e47c52b6a686e
tunateb <tunateb@gmail.com>
2020-06-27 01:39:22 +0300

    create hover effects on post images. fix follow btn

--- src/app/components/profile-card/profile-card.component.ts
+++ src/app/components/profile-card/profile-card.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileCardComponent implements OnInit {
 
+  following = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onFollow() {
+    this.following = !this.following;
   }
 
 }
